4032cfcfc2905e6718e77a7cba74ea01624f01ce
Neve Callaway <nevecallaway@gmail.com>
2025-01-29 23:14:17 -0700

    Passed Bishop

--- shared/src/main/java/chess/BishopMoveCalculator.java
+++ shared/src/main/java/chess/BishopMoveCalculator.java
@@ -7,6 +7,47 @@ public class BishopMoveCalculator implements ChessPieceMoveCalculator {
     @Override
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position) {
         Collection<ChessMove> validMoves = new ArrayList<>();
+
+        // Bishop potential moves: diagonals
+        int[] rowMoves = {1, 1, -1, -1};
+        int[] columnMoves = {1, -1, -1, 1};
+
+        // Iterate through all 4 diagonal directions
+        for (int i = 0; i < 4; i++) {
+            int row = position.getRow();
+            int column = position.getColumn();
+
+            while (true) {
+                row += rowMoves[i];
+                column += columnMoves[i];
+
+                if (!onBoard(row, column)) {
+                    break;
+                }
+
+                ChessPosition newPosition = new ChessPosition(row, column);
+                ChessPiece pieceAtNewPosition = board.getPiece(newPosition);
+
+                // If new position empty, valid, continue
+                if (pieceAtNewPosition == null) {
+                    validMoves.add(new ChessMove(position, newPosition, null));
+                }
+                // If new position has opposing color piece, valid but stop
+                else if (pieceAtNewPosition.getTeamColor() != board.getPiece(position).getTeamColor()) {
+                    validMoves.add(new ChessMove(position, newPosition, null));
+                    break;
+                }
+
+                else {
+                    break;
+                }
+            }
+        }
         return validMoves;
     }
+
+    private boolean onBoard(int row, int column) {
+        return row >= 1 && row <= 8 && column >= 1 && column <= 8;
+    }
 }
+
--- shared/src/main/java/chess/QueenMoveCalculator.java
+++ shared/src/main/java/chess/QueenMoveCalculator.java
@@ -7,6 +7,49 @@ public class QueenMoveCalculator implements ChessPieceMoveCalculator {
     @Override
     public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position) {
         Collection<ChessMove> validMoves = new ArrayList<>();
+
+        // Queen: rook & bishop
+        int[] rowMoves = {1, -1, 0, 0, 1, -1, -1, 1};
+        int[] columnMoves = {0, 0, -1, 1, -1, 1, 1, -1};
+
+        // Iterate through all 8 directions
+        for (int i = 0; i < 8; i++) {
+            int row = position.getRow();
+            int column = position.getColumn();
+
+            // Move in the current direction until colliding with the edge of the board or another piece
+            while (true) {
+                row += rowMoves[i];
+                column += columnMoves[i];
+
+                // If the position is off board, stop
+                if (!onBoard(row, column)) {
+                    break;
+                }
+
+                ChessPosition newPosition = new ChessPosition(row, column);
+                ChessPiece pieceAtNewPosition = board.getPiece(newPosition);
+
+                // If empty, valid, continue
+                if (pieceAtNewPosition == null) {
+                    validMoves.add(new ChessMove(position, newPosition, null));
+                }
+                // If new position has opposing color piece, valid but stop movement
+                else if (pieceAtNewPosition.getTeamColor() != board.getPiece(position).getTeamColor()) {
+                    validMoves.add(new ChessMove(position, newPosition, null));
+                    break;
+                }
+                // If new position has same color piece, end movement
+                else {
+                    break;
+                }
+            }
+        }
+
         return validMoves;
+    }
+
+    private boolean onBoard(int row, int column) {
+        return row >= 1 && row <= 8 && column >= 1 && column <= 8;
     }
 }
