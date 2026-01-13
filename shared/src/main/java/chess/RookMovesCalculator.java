44dc7fd27e4462ef7833a030b5dfe1cee60caa01
Neve Callaway <nevecallaway@gmail.com>
2026-01-13 14:51:29 -0700

    Updated rook, king using helper functions

--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -13,28 +13,15 @@ public class RookMovesCalculator implements ChessMovesCalculator {
         int[] colMoves = {-1, 0, 1, 0};
 
         for (int i = 0; i < 4; i++) {
-            int row = startRow;
-            int col = startCol;
-            while (true) {
-                row += rowMoves[i];
-                col += colMoves[i];
+            int row = startRow + rowMoves[i];
+            int col = startCol + colMoves[i];
 
-                if (!onBoard(row, col)) {
-                    break;
-                }
-                ChessPosition newPosition = new ChessPosition(row, col);
-                ChessPiece newPiece = board.getPiece(newPosition);
-                if (newPiece == null || newPiece.getTeamColor() != board.getPiece(position).getTeamColor()) {
-                    validMoves.add(new ChessMove(position, newPosition, null));
-                } else {
-                    break;
-                }
+            ChessPosition newPosition = new ChessPosition(row, col);
+            ChessPiece newPiece = board.getPiece(newPosition);
+            if (ChessPiece.validateMove(newPosition, newPiece, board.getPiece(position))) {
+                validMoves.add(new ChessMove(position, newPosition, null));
             }
         }
         return validMoves;
-    }
-
-    private boolean onBoard(int row, int col) {
-        return row >= 1 && row <= 8 && col >= 1 && col <= 8;
     }
 }
