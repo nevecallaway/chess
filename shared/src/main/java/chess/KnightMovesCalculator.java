d0d8334456986c194a85ef8cab91b7c5e38074b2
Neve Callaway <nevecallaway@gmail.com>
2026-01-13 14:40:18 -0700

    Finished KnightMovesCalculator, simplified static helper functions included in ChessPiece class

---
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -0,0 +1,26 @@
+package chess;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class KnightMovesCalculator implements ChessMovesCalculator {
+    @Override
+    public Collection<ChessMove> calculateMoves(ChessBoard board, ChessPosition position) {
+        Collection<ChessMove> validMoves = new ArrayList<>();
+        int startRow = position.getRow();
+        int startCol = position.getColumn();
+        int[] rowMoves = {-2, -1, 1, 2, 2, 1, -1, -2};
+        int[] colMoves = {1, 2, 2, 1, -1, -2, -2, -1};
+
+        for (int i = 0; i < 8; i++) {
+            int row = startRow + rowMoves[i];
+            int col = startCol + colMoves[i];
+            ChessPosition newPosition = new ChessPosition(row, col);
+            ChessPiece newPiece = board.getPiece(newPosition);
+            if (ChessPiece.validateMove(newPosition, newPiece, board.getPiece(position))) {
+                    validMoves.add(new ChessMove(position, newPosition, null));
+            }
+        }
+        return validMoves;
+    }
+}
