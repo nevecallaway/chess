a4f5a42d5d4b49975c61c56e7a24a0820337571d
Neve Callaway <nevecallaway@gmail.com>
2026-03-20 17:30:10 -0600

    Updated clientmain to show the board from each perspective for verification

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,10 +1,16 @@
 package client;
 
 import chess.*;
+import ui.ChessBoardUI;
 
 public class ClientMain {
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        ChessGame game = new ChessGame();
+        
+        System.out.println("WHITE PERSPECTIVE");
+        ChessBoardUI.displayBoardWhitePerspective(game);
+        
+        System.out.println("BLACK PERSPECTIVE");
+        ChessBoardUI.displayBoardBlackPerspective(game);
     }
 }
